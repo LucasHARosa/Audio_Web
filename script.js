eb9ad9ff0b45eec064d6d0683ee820f038287416
LucasHARosa <l.ucas.henrique.a.r@gmail.com>
2024-05-06 11:42:34 -0300

    Update funcionalidade 06/05

--- script.js
+++ script.js
@@ -1,69 +1,98 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const recordButton = document.getElementById('recordButton');
-    const deleteButton = document.getElementById('deleteButton');
-    const sendButton = document.getElementById('sendButton');
-    const recordedAudio = document.getElementById('recordedAudio');
-    let mediaRecorder;
-    let recordedChunks = [];
-  
-    
-  
-    // Gravar ou encerrar gravação de áudio
-    let isRecording = false;
-    recordButton.addEventListener('click', function() {
-        if (!isRecording) {
-            startRecording();
-        } else {
-            stopRecording();
-        }
-    });
+	const controlButton = document.getElementById('controlButton');
+	const iconControlButton = document.getElementById('iconControlButton');
+	const sendButton = document.getElementById('sendButton');
+	const recordedAudio = document.getElementById('recordedAudio');
 
-    // Apagar áudio
-    deleteButton.addEventListener('click', function() {
-        recordedAudio.src = '';
-        console.log('Gravação apagada.');
-    });
+	let mediaRecorder;
+	let recordedChunks = [];
+	let control = "empty";
+	
+	sendButton.disabled = true;
+	
+	
+	// Gravar/encerrar/limpar a gravação de áudio 
+	controlButton.addEventListener('click', function() {
+		if (control == "empty") {
+			startRecording();
+			iconControlButton.classList.remove('fa-microphone');
+			iconControlButton.classList.add('fa-stop');
+		}
+		else if (control == "recording") {
+			stopRecording();
+			iconControlButton.classList.remove('fa-stop');
+			iconControlButton.classList.add('fa-trash-alt');
+		} 
+		else if (control == "hasAudio") {
+			deleteAudio();
+			iconControlButton.classList.remove('fa-trash-alt');
+			iconControlButton.classList.add('fa-microphone');
+		}
+		updateButtonsState();
+	});
 
-    // Enviar áudio para API
-    sendButton.addEventListener('click', function() {
-        // Adicione sua lógica para enviar o áudio para a API aqui
-        // Por exemplo, use fetch() para enviar uma solicitação POST para a API com o áudio
-        const recordedAudioUrl = recordedAudio.src;
-        console.log('URL da gravação:', recordedAudioUrl);
-    });
-  
-    function startRecording() {
-        navigator.mediaDevices.getUserMedia({ audio: true })
-            .then(function(stream) {
-                mediaRecorder = new MediaRecorder(stream);
-                mediaRecorder.start();
-                console.log('Gravação iniciada.');
-  
-                mediaRecorder.ondataavailable = function(e) {
-                    recordedChunks.push(e.data);
-                };
-  
-                mediaRecorder.onstop = function() {
-                    const blob = new Blob(recordedChunks, { type: 'audio/mp4' });
-                    const url = URL.createObjectURL(blob);
-                    recordedAudio.src = url;
-                };
-            })
-            .catch(function(err) {
-                console.error('Erro ao acessar o dispositivo de áudio.', err);
-            });
-        recordButton.classList.add('recording');
-        isRecording = true;
-    }
-  
-    function stopRecording() {
-        if (mediaRecorder && mediaRecorder.state !== 'inactive') {
-            mediaRecorder.stop();
-            console.log('Gravação encerrada.');
-            recordButton.classList.remove('recording');
-            isRecording = false;
-            recordedChunks = [];
-        }
-    }
+
+	// Enviar áudio para API
+	sendButton.addEventListener('click', function() {
+		// Adicione sua lógica para enviar o áudio para a API aqui
+		// Por exemplo, use fetch() para enviar uma solicitação POST para a API com o áudio
+		const recordedAudioUrl = recordedAudio.src;
+		console.log('URL da gravação:', recordedAudioUrl);
+		hideButtons();
+	});
+
+	function startRecording() {
+		navigator.mediaDevices.getUserMedia({ audio: true })
+			.then(function(stream) {
+				mediaRecorder = new MediaRecorder(stream);
+				mediaRecorder.start();
+				console.log('Gravação iniciada.');
+
+				mediaRecorder.ondataavailable = function(e) {
+					recordedChunks.push(e.data);
+				};
+
+				mediaRecorder.onstop = function() {
+					const blob = new Blob(recordedChunks, { type: 'audio/mp4' });
+					const url = URL.createObjectURL(blob);
+					recordedAudio.src = url;
+				};
+			})
+			.catch(function(err) {
+				console.error('Erro ao acessar o dispositivo de áudio.', err);
+			});
+		
+		control = "recording";
+	}
+
+	function stopRecording() {
+		if (mediaRecorder && mediaRecorder.state !== 'inactive') {
+			mediaRecorder.stop();
+			console.log('Gravação encerrada.');
+			control = "hasAudio";
+			recordedChunks = [];
+		}
+	}
+
+	function deleteAudio() {
+		recordedAudio.src = '';
+		control = "empty";
+		console.log('Gravação apagada.');
+	}
+
+	function updateButtonsState() {
+		if (control === "hasAudio") {
+			sendButton.disabled = false;
+		} else {
+			sendButton.disabled = true;
+		}
+	}
+
+
+	// Esconder os botões de enviar e controlar áudio
+	function hideButtons() {
+		sendButton.style.display = 'none';
+		controlButton.style.display = 'none';
+	}
   
 });
